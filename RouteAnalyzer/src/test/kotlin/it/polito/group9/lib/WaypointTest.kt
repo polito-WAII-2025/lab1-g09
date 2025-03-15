89724e1fd2d47f7afbae09428e2bf660da4bf7de
Simone Nicol <s322782@studenti.polito.it>
2025-03-15 12:46:23 +0100

    test: Add tests.

---
+++ RouteAnalyzer/src/test/kotlin/it/polito/group9/lib/WaypointTest.kt
@@ -0,0 +1,53 @@
+package it.polito.group9.lib
+
+import it.polito.group9.model.WayPoint
+import org.junit.jupiter.api.Tag
+import java.io.StringReader
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class WaypointTest {
+    @Tag("normalizeTimestamp")
+    @Test
+    fun normalizeTimestampShouldReturnTimestampWhenHasDecimal() {
+        // Arrange
+        val timestamp = "123.456"
+
+        // Act
+        val result = normalizeTimestamp(timestamp)
+
+        // Assert
+        assertEquals("123", result, "Result should not contain decimals")
+    }
+
+    @Tag("normalizeTimestamp")
+    @Test
+    fun normalizeTimestampShouldReturnTimestampWhenNoDecimal() {
+        // Arrange
+        val timestamp = "123"
+
+        // Act
+        val result = normalizeTimestamp(timestamp)
+
+        // Assert
+        assertEquals("123", result, "Result should not contain decimals")
+    }
+
+    @Tag("readWaypointsFromCsv")
+    @Test
+    fun readWaypointsFromCsvShouldReturnListOfWaypoints() {
+        // Arrange
+        val bufferedReader = StringReader("123.456;45.678;9.012\n123.456;45.678;9.012\n123.456;45.678;9.012").buffered()
+        val expectedResult = listOf(
+            WayPoint(123, 45.678, 9.012),
+            WayPoint(123, 45.678, 9.012),
+            WayPoint(123, 45.678, 9.012)
+        )
+
+        // Act
+        val result = readWaypointsFromCsv(bufferedReader)
+
+        // Assert
+        assertEquals(expectedResult, result, "Result does not match expected result")
+    }
+}
