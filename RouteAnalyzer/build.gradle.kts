b080954d479c697e55c8504306da76ab718a9184
Simone Nicol <s322782@studenti.polito.it>
2025-03-23 14:26:15 +0100

    Update RouteAnalyzer/build.gradle.kts

--- RouteAnalyzer/build.gradle.kts
+++ RouteAnalyzer/build.gradle.kts
@@ -41,8 +41,8 @@ application {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_23
+    targetCompatibility = JavaVersion.VERSION_23
 }
 
 tasks.withType(Test::class) {
