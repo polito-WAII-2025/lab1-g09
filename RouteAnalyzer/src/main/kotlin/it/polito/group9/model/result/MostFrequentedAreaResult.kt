1435e832903b3b8f14cd606a6f31e97ba68e5539
Simone Nicol <s322782@studenti.polito.it>
2025-03-23 15:42:14 +0100

    chore: rename file.

--- RouteAnalyzer/src/main/kotlin/it/polito/group9/model/result/MostFrequentedAreaResult.kt
+++ RouteAnalyzer/src/main/kotlin/it/polito/group9/model/result/MostFrequentedAreaResult.kt
@@ -4,7 +4,7 @@ import it.polito.group9.model.WayPoint
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class MostVisitedAreaResult(
+data class MostFrequentedAreaResult(
     val centralWayPoint: WayPoint,
     val areaRadiusKm: Double,
     val entriesCount: Int,
--- RouteAnalyzer/src/main/kotlin/it/polito/group9/model/result/OutputResult.kt
+++ RouteAnalyzer/src/main/kotlin/it/polito/group9/model/result/OutputResult.kt
@@ -2,5 +2,5 @@ package it.polito.group9.model.result
 
 data class OutputResult (
     val maxDistanceFromStart: MaxDistanceFromStartResult,
-    val mostFrequentedArea: MostVisitedAreaResult,
+    val mostFrequentedArea: MostFrequentedAreaResult,
 )
